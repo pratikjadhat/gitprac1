ee2545e06d5a54a8042ca7a7d2c92c86bd723498
Pratik Jadhar <pratikjadhar2326@gmail.com>
2025-03-01 02:32:50 +0530

    updated

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+    
+     root 'pages#welcome'
+
+end
+
+# Rails.application.routes.draw do
+  
+#     root 'pages#home'
+  
+#     get 'about', to: 'pages#about'
+    
+#   end
+  
